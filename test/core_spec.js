e93fcbf97de4944e4db40bce0ec66ac0597bf3e2
sleahy115 <sleahy115@gmail.com>
2017-08-22 17:50:46 -0600

    create and pass second test case

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,16 +1,32 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-
-import {setEntries} from '../src/core';
+import {setEntries, next} from '../src/core';
 
 describe('application logic', () => {
   describe('setEntries', () => {
     it('converts to immutable', () => {
       const state = Map();
-      const entries = ['Trainspotting', '28 Days Later'];
+      const entries = ['Parks and Rec', '30 Rock'];
       const nextState = setEntries(state, entries);
       expect(nextState).to.equal(Map({
-        entries: List.of('Trainspotting', '28 Days Later')
+        entries: List.of('Parks and Rec', '30 Rock')
+      }));
+    });
+  });
+});
+
+describe('application logic', () => {
+  describe('next', () =>  {
+    it('takes the next two entries under vote', () => {
+      const state = Map({
+        entries: List.of('Parks and Rec', '30 Rock', 'Veronica Mars')
+      });
+      const nextState = next(state);
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Parks and Rec', '30 Rock')
+      }),
+      entries: List.of('Veronica Mars')
       }));
     });
   });
