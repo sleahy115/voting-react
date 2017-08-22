8c37be8bd4f94ca2643261524f0518d9792405ea
sleahy115 <sleahy115@gmail.com>
2017-08-22 12:47:49 -0600

    second sample test

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -1,19 +1,29 @@
 import {expect} from 'chai';
+import {List} from 'immutable';
 
 describe('immutability', () => {
 
-  describe('a number', () => {
+  // ...
 
-    function increment(currentState) {
-      return currentState + 1;
+  describe('A List', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.push(movie);
     }
 
     it('is immutable', () => {
-      let state = 42;
-      let nextState = increment(state);
+      let state = List.of('Trainspotting', '28 Days Later');
+      let nextState = addMovie(state, 'Sunshine');
 
-      expect(nextState).to.equal(43);
-      expect(state).to.equal(42);
+      expect(nextState).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later',
+        'Sunshine'
+      ));
+      expect(state).to.equal(List.of(
+        'Trainspotting',
+        '28 Days Later'
+      ));
     });
 
   });
