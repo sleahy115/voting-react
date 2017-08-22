130da7eff629314a8d7ab67372a5bc5f65a4fe09
sleahy115 <sleahy115@gmail.com>
2017-08-22 13:45:59 -0600

    run multiple tests

--- test/immutable_spec.js
+++ test/immutable_spec.js
@@ -29,3 +29,41 @@ describe('immutability', () => {
   });
 
 });
+
+describe('immutability', () => {
+
+  // ...
+
+  describe('a tree', () => {
+
+    function addMovie(currentState, movie) {
+      return currentState.set(
+        'movies',
+        currentState.get('movies').push(movie)
+      );
+    }
+
+    it('is immutable', () => {
+      let state = Map({
+        movies: List.of('Trainspotting', '28 Days Later')
+      });
+      let nextState = addMovie(state, 'Sunshine');
+
+      expect(nextState).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later',
+          'Sunshine'
+        )
+      }));
+      expect(state).to.equal(Map({
+        movies: List.of(
+          'Trainspotting',
+          '28 Days Later'
+        )
+      }));
+    });
+
+  });
+
+});
