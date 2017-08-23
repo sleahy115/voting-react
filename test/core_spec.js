e99d05e97857fce806d73e5813da96bfa612164a
sleahy115 <sleahy115@gmail.com>
2017-08-22 18:17:29 -0600

    create and pass 3rd custom test, to tally votes

--- test/core_spec.js
+++ test/core_spec.js
@@ -1,8 +1,10 @@
 import {List, Map} from 'immutable';
 import {expect} from 'chai';
-import {setEntries, next} from '../src/core';
+import {setEntries, next, vote} from '../src/core';
 
-describe('application logic', () => {
+describe('set entries', () => {
+  // creates entries list
+
   describe('setEntries', () => {
     it('converts to immutable', () => {
       const state = Map();
@@ -15,7 +17,8 @@ describe('application logic', () => {
   });
 });
 
-describe('application logic', () => {
+describe('next entry', () => {
+  // adds to entries list, moves to new state
   describe('next', () =>  {
     it('takes the next two entries under vote', () => {
       const state = Map({
@@ -27,6 +30,53 @@ describe('application logic', () => {
           pair: List.of('Parks and Rec', '30 Rock')
       }),
       entries: List.of('Veronica Mars')
+      }));
+    });
+  });
+});
+
+describe('vote', () => {
+  //adds vote tally
+  describe('vote', () => {
+    it('creates a tally of the votes entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Parks and Rec', '30 Rock')
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Parks and Rec');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Parks and Rec', '30 Rock'),
+          tally: Map({
+            'Parks and Rec': 1
+          })
+        }),
+        entries: List()
+      }));
+    });
+    it('adds to existing tally for the voted entry', () => {
+      const state = Map({
+        vote: Map({
+          pair: List.of('Parks and Rec', '30 Rock'),
+          tally: Map({
+            'Parks and Rec': 2,
+            '30 Rock' : 3
+          })
+        }),
+        entries: List()
+      });
+      const nextState = vote(state, 'Parks and Rec');
+      expect(nextState).to.equal(Map({
+        vote: Map({
+          pair: List.of('Parks and Rec', '30 Rock'),
+          tally: Map({
+            'Parks and Rec': 3,
+            '30 Rock': 3
+          })
+        }),
+        entries: List()
       }));
     });
   });
